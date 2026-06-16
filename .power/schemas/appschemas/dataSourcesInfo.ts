e694a11e95ed6f992422fa5da47e0ad78dcf40a4
Konstantin Ippolitov <ippolitk@mskcc.org>
2026-06-16 01:51:24 -0400

    initial commit

---
+++ .power/schemas/appschemas/dataSourcesInfo.ts
@@ -0,0 +1,22 @@
+/*!
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ * This file is auto-generated. Do not modify it manually.
+ * Changes to this file may be overwritten.
+ */
+
+export const dataSourcesInfo = {
+  "ppa_intakelogs": {
+    "tableId": "",
+    "version": "",
+    "primaryKey": "ppa_intakelogid",
+    "dataSourceType": "Dataverse",
+    "apis": {}
+  },
+  "ppa_medications": {
+    "tableId": "",
+    "version": "",
+    "primaryKey": "ppa_medicationid",
+    "dataSourceType": "Dataverse",
+    "apis": {}
+  }
+};
